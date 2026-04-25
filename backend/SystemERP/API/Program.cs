eb28d7e524659556b4953993facbdfea43d43f8c
cardozogirbydev <200120836+cardozogirbydev@users.noreply.github.com>
2026-04-25 17:17:09 -0400

    feat(backend): setup Entity Framework Core with SQL Server

--- backend/SystemERP/API/Program.cs
+++ backend/SystemERP/API/Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("DefaultConnection not found");
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SystemDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
@@ -17,9 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
